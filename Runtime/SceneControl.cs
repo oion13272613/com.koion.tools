e3c0c89714281df49af631bae3df53f9feee93f5
oion13272613 <o913272613@gmail.com>
2024-11-07 11:05:42 +0800

    Initial commit

---
+++ Runtime/SceneControl.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Koion
+{
+    /// <summary>
+    /// 控制場景，載入場景、結束應用程式
+    /// </summary>
+    public class SceneControl : MonoBehaviour
+    {
+        /// <summary>
+        /// 載入場景
+        /// </summary>
+        /// <param name="_sceneName">載入的場景名稱</param>
+        public void LoadScene(string _sceneName)
+        {   
+            SceneManager.LoadScene(_sceneName);
+        }
+
+        /// <summary>
+        /// 結束應用程式
+        /// </summary>
+        public void QuitGame()
+        {   
+            Application.Quit();
+        }
+    }
+
+}
